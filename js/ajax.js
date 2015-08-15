b1f8c2ad83917e8869074905f65ee6cda844f74e
canevous regardepas <canevorepa@gmail.com>
2015-08-15 12:27:56 +0200

    Modification de HD_

--- js/ajax.js
+++ js/ajax.js
@@ -1,39 +1,42 @@
 familyDocs.ajax = (function() {
 
-    // var _source = "http://canevorepa.free.fr/familydocs/"; // prod
-    var _source = "http://localhost/imagenotes/"; // local
+    var _source = familyDocs.debugMode ?
+        "http://localhost/imagenotes/" :
+        "http://canevorepa.free.fr/imagenotes/";
+
     var _service = {
         getDocumentsNames : "getdocumentnames.php",
         getdocument : "getdocument.php"
     };
 
+    function _buildGetValueRequest(service, name, value) {
+        return _source + service + "?" + name + "=" + value;
+    }
+
+    function _findRequestValue(request) {
+        return request.split("=")[1];
+    }
+
+    function _buildUrl(service) {
+        return _source + service;
+    }
+
     return {
-        buildGetValueRequest : function(service, name, value) {
-            return _source + service + "?" + name + "=" + value;
-        },
-
-        findRequestValue : function(request) {
-            return request.split("=")[1];
-        },
-
-        buildUrl : function(service) {
-            return _source + service;
-        },
 
         getDocumentsNames : function(onSuccess, onError, onFinished) {
-            HD_.Ajax.makeRequest('GET', familyDocs.ajax.buildUrl(_service.getDocumentsNames), onSuccess, onError, onFinished);
+            HD_.Ajax.makeRequest('GET', _buildUrl(_service.getDocumentsNames), onSuccess, onError, onFinished);
         },
 
         getDocuments : function(imgNames, processElement, onFinished) {
             var urls = [];
             imgNames.forEach(function(imgNames) {
-                var request = familyDocs.ajax.buildGetValueRequest(_service.getdocument, "docname", imgNames);
+                var request = _buildGetValueRequest(_service.getdocument, "docname", imgNames);
                 urls.push(request);
                 console.log("req: " + request);
             });
 
             HD_.Ajax.chainRequests("GET", urls, function onSuccess(request, responseText) {
-                var reqval = familyDocs.ajax.findRequestValue(request);
+                var reqval = _findRequestValue(request);
                 console.log("reqval: " + reqval);
 
                 processElement(reqval, responseText);
--- js/familydocs.js
+++ js/familydocs.js
@@ -1,8 +1,13 @@
 var familyDocs = (function(){
 
     return {
+        debugMode : HD_.Debug.isLocalHost(),
 
         init : function() {
+            if (familyDocs.debugMode) {
+                HD_.Debug.persistentLocalWarnings();
+            }
+
             var docList = familyDocs.documents.create();
             docList.loadFromDatabase();
         }
