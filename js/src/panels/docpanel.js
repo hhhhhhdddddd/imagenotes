90405883a04b1b62532cb8c08dd2fdb702bf08dc
canevous regardepas <canevorepa@gmail.com>
2015-08-19 20:03:15 +0200

    Utilisation de la nouvelle interface des panels de HD_

--- js/src/panels/docpanel.js
+++ js/src/panels/docpanel.js
@@ -2,33 +2,35 @@ imageNotes.docPanel = (function() {
 
     return {
         create : function(description, imgSource, thumbnailSource) {
-            var buttonsPanel = HD_.HorizontalPanel.create([
-                HD_.PanelField.create({
-                    name: "enlarge",
-                    type: "button",
-                    innerLabel: "Enlarge",
-                    handler : function() {
-                        window.open(imgSource, "Image");
-                    }
-                })
-            ], "buttonspanel");
+            var buttonsPanel = HD_.HorizontalPanel.create({name: "buttonspanel"});
+            buttonsPanel.pushPanelElement(HD_.PanelField.create({
+                name: "enlarge",
+                type: "button",
+                innerLabel: "Enlarge",
+                handler : function() {
+                    window.open(imgSource, "Image");
+                }
+            }));
 
-            var docPanel = HD_.VerticalPanel.create([
-                HD_.HorizontalPanel.create([
-                    HD_.PanelField.create({
-                        name: "imagepanel",
-                        type: "image",
-                        initValue: thumbnailSource
-                    }),
-                    HD_.PanelField.create({
-                        name: "descriptionpanel",
-                        type: "textDisplay",
-                        initValue: description,
-                        style: {verticalAlign:"top"}
-                    })
-                ], "docpanel"),
-                buttonsPanel],
-                "docpanel");
+            var docElementsPanel = HD_.HorizontalPanel.create({name: "docelementspanel"});
+            docElementsPanel.pushPanelElement(HD_.PanelField.create({
+                name: "imagepanel",
+                type: "image",
+                initValue: thumbnailSource
+            }));
+            docElementsPanel.pushPanelElement(HD_.PanelField.create({
+                name: "descriptionpanel",
+                type: "textDisplay",
+                initValue: description,
+                style: {verticalAlign:"top"}
+            }));
+
+            var sourceSplit = imgSource.split("/");
+            var title = sourceSplit[sourceSplit.length - 1];
+            console.log("title: " + title);
+            var docPanel = HD_.VerticalPanel.create({name: "docpanel", title: title});
+            docPanel.pushPanelElement(docElementsPanel);
+            docPanel.pushPanelElement(buttonsPanel);
 
             return docPanel;
         }
--- js/src/panels/mainpanel.js
+++ js/src/panels/mainpanel.js
@@ -2,10 +2,10 @@ imageNotes.mainPanel = (function() {
 
     return {
         create : function(typeOfInputValues, textVersions) {
-            var mainPanel = HD_.VerticalPanel.create([], "mainPanel");
+            var mainPanel = HD_.VerticalPanel.create({name: "mainPanel"});
 
             mainPanel.onAddDocument = function(doc) {
-                mainPanel.addAndShow(imageNotes.docPanel.create(
+                mainPanel.pushAndShow(imageNotes.docPanel.create(
                     doc.getDescription(),
                     doc.getImageSource(),
                     doc.getThumbnailSource())
